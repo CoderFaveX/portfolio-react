238bd73a822e46064df9b791b18e9cb078550022
CoderFaveX <faveejiofor2009@gmail.com>
2024-06-13 18:56:54 +0100

    saved changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { createRoot } from "react-dom/client";
 
-const root = createRoot(document.getElementById("root"))
-root.render(<App />);
+ReactDOM.render(<App />, document.getElementById("root"));
